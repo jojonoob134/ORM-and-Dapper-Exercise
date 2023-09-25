6ecc4e8f1e4db5b318b49c4330472ee1b6138607
Jorge Saldana <jojonoob134@gmail.com>
2023-09-25 11:59:32 -0500

    did first exercise

---
+++ ORM-Dapper/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class Department : IDepartmentRepository
+    {
+        public int DepartmentId { get; set; }
+
+        public string Name { get; set; }
+
+        public IQueryable<Department> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            departmentRepo.InsertDepartments("jorge's new department");
+
+            var depaetments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in depaetments)
+            {
+                Console.WriteLine(department.Name);
+                Console.WriteLine(department.DepartmentId);
+                Console.WriteLine();
+            }
         }
     }
 }
